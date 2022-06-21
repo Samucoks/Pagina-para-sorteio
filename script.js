f61ca8e236e82f1d0473c7f14603c2ae90e1d1a2
Pedro Samuel <caminhadadev@gmail.com>
2022-06-21 08:34:16 -0300

    criação do projeto

---
+++ script.js
@@ -0,0 +1,18 @@
+let botao = document.getElementById('botao')
+
+let participantes = ['Pedro', 'Lucas', 'Marta', 'Letícia', 'Maria', 'Luana', 'Vitória', 'Augusto', 'Leandro', 'Marcelo', 'Lucas']
+botao.onclick = ()=>{
+    let Mnumber = Math.round(document.getElementById('Mnumber').value)
+    let result= document.getElementById('result')
+    let winner = document.getElementById('winner')
+    let sorteio = Math.round(Math.random() * Mnumber)
+    result.innerHTML = 'O número sorteado foi: ' + sorteio
+    result.style.color ='blue'
+    result.style.fontSize = '20px'
+    if(sorteio < participantes.length){
+    winner.innerHTML = "O nome do(a) Vencedor(a) é: "+participantes[sorteio]
+    }
+    else{
+        winner.innerHTML = ''
+    }
+}
